228262427a3d0ef39e333edbcde71592d95011c3
monarchxr <raunakjais2131@gmail.com>
2026-03-16 14:57:13 +0530

    added naive concurrency method

---
+++ naive_concurrency/naive_concurr.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func processTask(task int, wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	duration := time.Duration(rand.Intn(41)+10) * time.Millisecond
+
+	time.Sleep(duration)
+
+	ans := task * task
+	ans = ans
+}
+
+func main() {
+
+	startTime := time.Now()
+
+	//ok so this a naive version of concurrent processing
+
+	// we'll be working with goroutines
+	// whats a goroutine?
+
+	// a lightweight independent thread
+	// enables easy concurrency
+
+	// how to make goroutine?
+	// just prefix the function call with go
+
+	// also we need a waitgroup before spawning each goroutine
+
+	var wg sync.WaitGroup
+	// a waitgroup is like a counter
+	// formally, a semaphore
+
+	// it waits for a group of goroutines to finish executing
+	// add increments the counter by input value
+	// done decrements the counter by 1
+	// wait - blocks main thread until counter hits 0
+
+	//lets begin
+
+	tasks := [1000]int{}
+
+	for i := 0; i < 1000; i++ {
+		tasks[i] = rand.Intn(20000)
+	}
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go processTask(tasks[i], &wg)
+	}
+	wg.Wait()
+
+	endTime := time.Since(startTime)
+	fmt.Printf("End time = %v\n", endTime)
+}
